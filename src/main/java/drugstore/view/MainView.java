6b2c71d45aa8c4dca43ff1ed6ec5d99eada13588
matibdr7 <matias.bdr7@gmail.com>
2026-09-08 02:35:10 -0300

    agregar vista principal

---
+++ src/main/java/drugstore/view/MainView.java
@@ -0,0 +1,13 @@
+package drugstore.view;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        add("DRUGSTORE \n Sistema de gestion");
+    }
+}
